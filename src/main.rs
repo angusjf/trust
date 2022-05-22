21d76fd85a60c88e98f2e0f958d487c3e22ae81b
Angus Findlay <angusf@theodo.co.uk>
2022-05-22 17:56:46 +0100

    separated io and drawin

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,44 @@ use std::path::Path;
 use std::io;
 use std::fs::{self, DirEntry};
 
-fn visit_dirs(dir: &Path) -> io::Result<()> {
-    println!("{}", dir.display());
+enum Tree<T> {
+    Leaf(T),
+    Node(T, Vec<Tree<T>>)
+}
+
+fn visible(entry: DirEntry) -> bool {
+    match entry.file_name().to_str() {
+        Some(file_name) => match file_name.chars().next() {
+            Some('.') => false,
+            None => false,
+            _ => true
+        },
+        None => false
+    }
+}
+
+fn visit_dirs(dir: &Path, depth: i32) -> io::Result<Tree<String>> {
+    let file_name_x = match dir.file_name().and_then(|x| x.to_str()) {
+        Some(file_name) => file_name.to_string(),
+        None => ".".to_string(),
+    };
+
     if dir.is_dir() {
-        let mut it = fs::read_dir(dir)?;
+        let mut kids = Vec::new();
+
+        let it = fs::read_dir(dir)?;
         for entry in it {
-            // if (it.peek().is_none()) {
-            //     print!("!\n");
-            // }
             let entry = entry?;
             let path = entry.path();
-            visit_dirs(&path)?;
+            if visible(entry) {
+                kids.push(visit_dirs(&path, depth + 1)?);
+            }
         }
+
+        Ok(Tree::Node(file_name_x, kids))
+    } else {
+        Ok(Tree::Leaf(file_name_x))
     }
-    Ok(())
 }
 
 fn main() -> Result<(), &'static str> {
@@ -29,8 +53,35 @@ fn main() -> Result<(), &'static str> {
 
         let path = Path::new(dirname);
 
-        visit_dirs(path);
+        match visit_dirs(path, 0) {
+            Ok(tree) => Ok(view(tree, &mut Vec::new())),
+            Err(_) => Err("file system error")
+        }
+    }
+}
 
-        Ok(())
+fn view(tree: Tree<String>, depth: &mut Vec<bool>) -> () {
+    match tree {
+        Tree::Leaf(name) => println!("{}{}", get_prefix(&depth), name),
+        Tree::Node(name, kids) => {
+            println!("{}{}", get_prefix(&depth), name);
+            depth.push(true);
+            for kid in kids {
+                view(kid, depth);
+            }
+            depth.pop();
+        }
+    }
+}
+
+fn get_prefix(n: &[bool]) -> String {
+    // (0..n.len()).map(|_| "    ").collect::<String>()
+    match n {
+        [] => "".to_string(),
+        _ => {
+            let n_rev = n.iter().rev();
+            n_rev.map(|b| if *b { "│   " } else { "    " } ).collect::<String>()
+            // String.concat (List.map (\b -> if b then "    " else "│   ") (List.reverse xs)) ++ (if x then "└── " else "├── ")
+        }
     }
 }
