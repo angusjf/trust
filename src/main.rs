cf36c7dfa4c1f89b75dc445f113aedc01e054c13
Angus Findlay <angusf@theodo.co.uk>
2022-05-22 20:52:48 +0100

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,27 @@
-use std::env;
+use std::env::{args};
 use std::path::Path;
 use std::io;
-use std::fs::{self, DirEntry};
+use std::fs::{read_dir, DirEntry};
 
 fn visible(entry: &DirEntry) -> bool {
-    match entry.file_name().to_str() {
-        Some(file_name) => match file_name.chars().next() {
-            Some('.') => false,
-            None => false,
-            _ => true
-        },
-        None => false
+    match entry.file_name().to_str().and_then(|file_name| file_name.chars().next()) {
+        None => false,
+        Some('.') => false,
+        _ => true
     }
 }
 
 fn visit_dirs(dir: &Path, depth: &mut Vec<bool>) -> io::Result<()> {
     let file_name_x = match dir.file_name().and_then(|x| x.to_str()) {
-        Some(file_name) => file_name.to_string(),
-        None => ".".to_string(),
-    };
+        Some(file_name) => Ok(file_name.to_string()),
+        None => Err(io::Error::new(io::ErrorKind::Other, "oh no!"))
+    }?;
+
+    println!("{}{}", get_prefix(&depth), file_name_x);
 
     if dir.is_dir() {
-        println!("{}{}", get_prefix(&depth), file_name_x);
 
-        let mut iter = fs::read_dir(dir)?.peekable();
+        let mut iter = read_dir(dir)?.peekable();
 
         while let Some(entry) = iter.next() {
             let entry = entry?;
@@ -33,34 +31,27 @@ fn visit_dirs(dir: &Path, depth: &mut Vec<bool>) -> io::Result<()> {
                 depth.pop();
             }
         }
-
-        Ok(())
-    } else {
-        println!("{}{}", get_prefix(&depth), file_name_x);
-
-        Ok(())
     }
+    
+    Ok(())
 }
 
-fn main() -> Result<(), &'static str> {
-    let args: Vec<String> = env::args().collect();
+fn main() -> Result<(), io::Error> {
+    let args: Vec<String> = args().collect();
 
     let dirname = if args.len() < 2 { "." } else { &args[1] };
 
     let path = Path::new(dirname);
 
-    match visit_dirs(path, &mut Vec::new()) {
-        Ok(()) => Ok(()),
-        Err(_x) => Err("file system error")
-    }
+    visit_dirs(path, &mut Vec::new())
 }
 
 fn get_prefix(n: &[bool]) -> String {
     match n.iter().last() {
         None => "".to_string(),
-        Some(b1) => {
+        Some(rightmost) => {
             let init = n.iter().take(n.len() - 1).map(|b| if *b { "    " } else { "│   " } ).collect::<String>();
-            let last = if *b1 { "└── " } else { "├── " };
+            let last = if *rightmost { "└── " } else { "├── " };
             format!("{}{}", init, last)
         }
     }
